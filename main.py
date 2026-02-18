a1cdf910659f23678f91ee4d863af6f53ece2d63
EddyTheGrey <eduardo.rodriguez.feo.ber@gmail.com>
2026-02-18 15:47:23 -0800

    chapter4

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 import constants
+import player 
+import asteroid
+import asteroidfield
 from logger import log_state
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} ")
@@ -8,7 +12,24 @@ def main():
     
     #initialize game
     pygame.init()
+
+    #Creating Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    player.Player.containers = (updatable,drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
+
+
+    clocks = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+    charater = player.Player(x = constants.SCREEN_WIDTH/2, y= constants.SCREEN_HEIGHT/2)
+    field = asteroidfield.AsteroidField()
+
+
 
     while True:
         log_state()
@@ -18,7 +39,18 @@ def main():
                 return
             pass
         screen.fill(color="black")
+
+        #Character actions
+        for X in updatable:
+            X.update(dt)
+        for X in drawable:
+            X.draw(screen)
+        
         pygame.display.flip()
+
+
+        dt = clocks.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
